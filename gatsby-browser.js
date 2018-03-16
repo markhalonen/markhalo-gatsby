769a521741752791232ce3cc5bd8d94ef644ab1b
Mark Halonen <halonen.mark@gmail.com>
2018-03-16 19:51:09 -0400

    Google analytics

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -5,3 +5,10 @@
  */
 
  // You can delete this file if you're not using it
+
+import ReactGA from 'react-ga';
+ReactGA.initialize('UA-94002757-1');
+
+exports.onRouteUpdate = (state, page, pages) => {
+  ReactGA.pageview(state.location.pathname);
+};
