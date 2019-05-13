662d714d5b6cdf51b442063b1b719861c565d999
Mark Halonen <halonen.mark@gmail.com>
2019-05-13 11:04:32 -0400

    update

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,10 +4,10 @@ import mapImage from '../images/map.png'
 
 const About = () => (
   <div>
-    <div style={{textAlign: 'center'}}>
-      <img src={mapImage} alt="Map" style={{width: '500px', align:"middle"}}/>
+    <div style={{ textAlign: 'center' }}>
+      <img src={mapImage} alt="Map" style={{ width: '500px', align: "middle" }} />
     </div>
-    <p>I reside in Brighton, Michigan and commute to Ann Arbor. When I'm not building software, I'm lifting weights, reading books, reading the internet, and socializing.</p>
+    <p>I reside in Brighton, Michigan. When I'm not building software, I'm lifting weights, reading books, reading the internet, and socializing.</p>
   </div>
 )
 
